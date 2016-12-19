e5283745d8db463a8ccca2701e355b87be8bbf5c
Dylan Vu <dylan.n.vu@gmail.com>
2016-12-19 00:53:45 -0800

    Send all guest records from JSON file

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 'use strict';
 
-// Import http module
+// Import modules
+const fs = require('fs');
 const http = require('http');
+const path = require('path');
+
+// assign file path
+const guestsPath = path.join(__dirname, 'guests.json');
 
 // Create server
 // for every request, the callback is invoked. req will contain the incoming
 // HTTP request object. res will contain an empty outgoing HTTP response object
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/guests') {
-    const guests = ['Roshella', 'Dylan'];
-    const guestsJSON = JSON.stringify(guests);
+    fs.readFile(guestsPath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err.stack);
 
-    res.setHeader('Content-Type', 'application/json');
-    res.end(guestsJSON);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Internal Server Error');
+
+        return;
+      }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(data);
+    });
   }
   else {
     res.statusCode = 404;
