162e791d482622cec4c6a4ebcfc843382e83adce
Dylan Vu <dylan.n.vu@gmail.com>
2016-12-19 03:40:09 -0800

    add express server

---
+++ serverExpress.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const guestsPath = path.join(__dirname, 'guests.json');
+
+const express = require('express');
+const app = express();
+
+app.disable('x-powered-by');
+
+app.use('/guests', (req, res) => {
+  fs.readFile(guestsPath, 'utf8', (err, guestsJSON) => {
+    if (err) {
+      console.error(err.stack);
+      res.sendStatus(500);
+
+      return;
+    }
+
+    const guests = JSON.parse(guestsJSON);
+
+    res.send(guests);
+  });
+});
+
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
