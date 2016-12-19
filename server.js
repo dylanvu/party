431430a1f71879b210f7d50e7e1c19f4a403e6f9
Dylan Vu <dylan.n.vu@gmail.com>
2016-12-19 00:20:07 -0800

    add basic HTTP server

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Import http module
+const http = require('http');
+
+// Create server
+// for every request, the callback is invoked. req will contain the incoming
+// HTTP request object. res will contain an empty outgoing HTTP response object
+const server = http.createServer((req, res) => {
+  const guests = ['Roshella', 'Dylan'];
+  const guestsJSON = JSON.stringify(guests);
+
+  res.setHeader('Content-Type', 'application/json');
+  res.end(guestsJSON);
+});
+
+const port = 8000;
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
